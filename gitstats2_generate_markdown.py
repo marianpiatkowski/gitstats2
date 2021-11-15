538bd11be1e3bb11a479fbeba20287f668a7431a
Marian Piatkowski <marianpiatkowski@web.de>
2021-11-15 19:02:34 +0100

    start filling the Activity-tab in the RMarkdown file generator

--- gitstats2_generate_markdown.py
+++ gitstats2_generate_markdown.py
@@ -2,6 +2,7 @@
 import datetime
 import time
 from string import Template
+from dateutil import rrule
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
@@ -71,6 +72,7 @@ class RMarkdownFile :
     def generate(self) :
         results = self.git_statistics.configuration.copy()
         self._fill_general(results)
+        self._fill_activity(results)
         self._fill_lines(results)
         self._fill_tags(results)
         out = self._template.substitute(results)
@@ -110,6 +112,63 @@ class RMarkdownFile :
             float(self.git_statistics.get_total_commits())/total_authors
         results['avg_commits_per_author'] = f"{avg_commits_per_author:.1f}"
 
+    def _fill_activity(self, results) :
+        activity_period_weeks = 32
+        results['activity_period_weeks'] = activity_period_weeks
+        self._plot_weekly_activity(activity_period_weeks)
+        results['weekly_activity_png'] = '![WeeklyActivity](weekly_activity.png)'
+        self._fill_hour_of_day_table(results)
+        self._plot_hour_of_day()
+        results['hour_of_day_png'] = '![HourOfDay](hour_of_day.png)'
+
+    def _plot_weekly_activity(self, activity_period_weeks) :
+        weeks = []
+        commits = []
+        now = datetime.datetime.now()
+        begin = now - datetime.timedelta(weeks=activity_period_weeks)
+        for date in rrule.rrule(rrule.WEEKLY, dtstart=begin, until=now) :
+            weeks.append(date)
+            yyw = date.strftime('%Y-%W')
+            commits.append(self.git_statistics.activity_by_year_week.get(yyw, 0))
+        plt.figure(figsize=(16.0, 6.0))
+        plt.fill_between(weeks, commits, step='post')
+        plt.xticks(
+            weeks,
+            map(lambda el : el.strftime('%Y-%W'), weeks),
+            rotation=90)
+        axes = plt.gca()
+        axes.set_ylabel('Commits')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig("weekly_activity.png")
+        plt.close()
+
+    def _fill_hour_of_day_table(self, results) :
+        activity_by_hour_of_day = self.git_statistics.get_activity_by_hour_of_day()
+        total_commits = sum(activity_by_hour_of_day.values())
+        hour_of_day_table = []
+        for i in range(0, 24) :
+            commits = activity_by_hour_of_day.get(i, 0)
+            hour_of_day_table.append([commits, f"{(100*commits/total_commits):.2f}"])
+        _df = pd.DataFrame(hour_of_day_table, columns=['Commits', '%'])
+        df_transposed = _df.transpose()
+        results['hour_of_day_table'] = \
+            df_transposed.to_markdown(tablefmt="github", numalign="center")
+
+    @staticmethod
+    def _plot_hour_of_day() :
+        plot_data = pd.read_csv('hour_of_day.csv', delimiter=', ', engine='python')
+        plt.figure(figsize=(16.0, 6.0))
+        plt.fill_between(plot_data.Hour, plot_data.Commits, step='post')
+        axes = plt.gca()
+        axes.set_ylabel('Commits')
+        axes.set_xlim(0,24)
+        axes.set_xticks(range(0,24))
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig("hour_of_day.png")
+        plt.close()
+
     def _fill_lines(self, results) :
         self._plot_lines_of_code()
         results['lines_of_code_png'] = '![LinesOfCode](lines_of_code.png)'
@@ -130,6 +189,7 @@ class RMarkdownFile :
         axes = plt.gca()
         axes.set_ylabel('Lines')
         plt.grid(True)
+        plt.tight_layout()
         plt.savefig("lines_of_code.png")
         plt.close()
 
@@ -145,6 +205,7 @@ class RMarkdownFile :
         axes = plt.gca()
         axes.set_ylabel('Lines')
         plt.grid(True)
+        plt.tight_layout()
         plt.savefig("lines_of_code_by_author.png")
         plt.close()
 
